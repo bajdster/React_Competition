d70970a97c38f0c4c5ef0980d021bac79b3336dc
Bajda <bajdster@gmail.com>
2023-07-20 22:10:53 +0100

    v1 serching for winner

--- react-competition/src/components/Players.js
+++ react-competition/src/components/Players.js
@@ -4,12 +4,15 @@ import ViewAgendaIcon from '@mui/icons-material/ViewAgenda';
 import { useSelector } from 'react-redux';
 import classes from "./Players.module.scss"
 import Player from "./Player"
+import { Button } from '@mui/material';
+import Winner from "../assets/youwin.gif"
 
 
 const Players = (props) => {
 
     const [agendaView, setAgendaView] = useState(false)
     const players = useSelector(state=> state.players.names)
+    const [winner, setWinner] = useState("")
 
     const viewChange = () =>
     {
@@ -17,6 +20,14 @@ const Players = (props) => {
         {
             return !prev;
         })
+    }
+
+    const winnerSearch = () =>
+    {
+        const playersAmount = players.length;
+        const random = Math.floor(Math.random() * playersAmount)
+
+        setWinner(players[random])
     }
 
   return (
@@ -41,6 +52,16 @@ const Players = (props) => {
           return <Player key={index} index={index} name ={name} agenda={agendaView}/> 
         }): <p>There is no added players</p>}
         </div>
+
+        <div className={classes.actionButton}>
+            {players.length> 0 &&<Button variant='text' sx={{ fontSize:"50px", color: 'rgb(13, 146, 173)', fontWeight:"bold", textShadow:"1px 1px 5px black"}} onClick={winnerSearch}>Let's find out!</Button>}
+        </div>
+        
+        <h2>{winner}</h2>
+        {winner !== '' && <div className={classes.winning}>
+        
+            <img src={Winner}></img>
+        </div>}
     </div>
   )
 }
