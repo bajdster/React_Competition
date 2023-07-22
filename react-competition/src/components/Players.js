f1e2c5fe78e1b824fb3c2d0974d7ddfc237411ba
Bajda <bajdster@gmail.com>
2023-07-22 18:46:39 +0100

    Added tooltips

--- react-competition/src/components/Players.js
+++ react-competition/src/components/Players.js
@@ -4,8 +4,9 @@ import ViewAgendaIcon from '@mui/icons-material/ViewAgenda';
 import { useSelector, useDispatch } from 'react-redux';
 import classes from "./Players.module.scss"
 import Player from "./Player"
-import { Button } from '@mui/material';
+import { Button, Checkbox, FormControlLabel } from '@mui/material';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import {Tooltip} from 'react-tooltip'
 
 import { compActions } from '../store/comp-slice';
 
@@ -90,31 +91,41 @@ const Players = (props) => {
 
     const resetCompetition = () =>
     {
-      const confirm = window.confirm("Do you want to reset competition?")
-      if(confirm) dispatch(compActions.resetState())
+      if(players.length > 0)
+      {
+        const confirm = window.confirm("Do you want to reset competition?")
+        if(confirm) dispatch(compActions.resetState())
+        
+      }
     }
 
   return (
   <div className={classes.playersBox}>
         <div className={classes.playersOptions}>
-            <GridViewIcon onClick={viewChange} sx={{color: agendaView ? "grey":"black",
+        <div className={classes.leftOptions}>
+            <Tooltip id="grid"/> 
+            <GridViewIcon data-tooltip-id="grid" data-tooltip-content="Grid View" onClick={viewChange} sx={{color: agendaView ? "grey":"black",
             '&:hover':  {
                 backgroundColor: 'transparent',
                 color: 'black',
                 cursor: 'pointer',
               }}}/>
-            <ViewAgendaIcon onClick={viewChange} sx={{color: agendaView ? "black":"grey",
+             <Tooltip id="agenda"/>  
+            <ViewAgendaIcon data-tooltip-id="agenda" data-tooltip-content="Agenda View" onClick={viewChange} sx={{color: agendaView ? "black":"grey",
             '&:hover':  {
                 backgroundColor: 'transparent',
                 color: 'black',
                 cursor: 'pointer',
               }}}/>
-              <RestartAltIcon sx={{color:"grey",
+              <Tooltip id="reset"/> 
+              <RestartAltIcon data-tooltip-id="reset" data-tooltip-content="Reset players" sx={{color:"grey",
             '&:hover':  {
                 backgroundColor: 'transparent',
                 color: 'black',
                 cursor: 'pointer',
               }}} onClick = {resetCompetition}/>
+          </div>
+              <FormControlLabel control={<Checkbox />} label="More winners"/>
         </div>
       <div className={classes.players}>
         {players.length> 0 ? props.names.map((name, index)=>
